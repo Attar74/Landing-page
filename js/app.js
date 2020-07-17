26362611c6593e4057021500e55b217cd9c62cd2
mahmod <mahmoudelattar800@gmail.com>
2020-07-17 18:53:39 +0200

    Reimplementing some functions

--- js/app.js
+++ js/app.js
@@ -18,8 +18,9 @@
  * 
 */
 
-const navigation = document.querySelector('header nav ul#navbar__list');
-const sections = document.querySelectorAll('section');
+const navigationBar = document.querySelector('header nav ul');
+const AllSections = document.querySelectorAll('section');
+
 
 /**
  * End Global Variables
@@ -32,56 +33,61 @@ const sections = document.querySelectorAll('section');
 */
 
 // build the nav
-const navBuilder = () =>{
-    let navUI = '';
-    sections.forEach(section => {
-        const sectionId = section.id;
-        const sectionDataNav = section.dataset.nav;
-        navUI += `<li><a class="menu__link" href="#${sectionId}">${sectionDataNav}</a></li>`;
-    });
-    navigation.innerHTML = navUI;
-    navigation.style = "color:red";
-}
-navBuilder();
+AllSections.forEach(section => {
+    const li = document.createElement('li');
+    li.innerHTML = `<a href='#${section.id}' class="${section.id}">${section.dataset.nav}</a>`;
+    navigationBar.append(li);
+});
+const linkStyle = "color : blue; text-decoration: none";
+let allLinks = document.querySelectorAll('li a');
+allLinks.forEach(link => {
+    link.style = linkStyle;
+});
 
 
 // Add class 'active' to section when near top of viewport
 
 //get the offset of the section
-const offset = (section) => {
+const offsetXY = (section) => {
     return Math.floor(section.getBoundingClientRect().top);
 };
 
 //remove the active class
-const removeActive = (condition,section)=> {
+const removeActiveClass = (condition,section)=> {
     if(!condition) {
         section.classList.remove('your-active-class');
         let style = "background-color:linear-gradient(0deg, rgba(136,203,171,1) 0%, rgba(0,13,60,1) 100%);";
         section.style.cssText = style;
+        let classNameOfa = section.id;
+        let a = document.querySelector("ul li a."+classNameOfa);
+        a.style = "color:blue; text-decoration:none";
     }
 }
 
 //add the active class
-const addActive = (condition, section) => {
+const addActiveClass = (condition, section) => {
     if(condition) {
         section.classList.add('your-active-class');
         let style = "background-color:rgba(136,150,171,1);";
         section.style.cssText = style;
+        let classNameOfa = section.id;
+        let a = document.querySelector("ul li a."+classNameOfa);
+        a.style = "color:red; text-decoration:none;";
     }
 }; 
 
 //implementing of setting the activitaion function 
 
-const sectionActivitaion = () =>{
-    sections.forEach(section => {
-        const elementOffset = offset(section);
-        const inviewport = () => elementOffset < 150 && elementOffset >= -480;
-        removeActive(inviewport(),section);
-        addActive(inviewport(),section);
+const isItActive = () =>{
+    AllSections.forEach(section => {
+        const elementOffset = offsetXY(section);
+        const isItInView = () => elementOffset < 150 && elementOffset >= -480;
+        removeActiveClass(isItInView(),section);
+        addActiveClass(isItInView(),section);
     })
 };
 
-window.addEventListener('scroll',sectionActivitaion);
+window.addEventListener('scroll',isItActive);
 
 /**
  * End Main Functions
